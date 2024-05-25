6ded1e217be0f8c1e6460da5022a6b3f4f302aee
Bikram Majhi <115028900+bikrammajhi@users.noreply.github.com>
2024-05-25 11:52:52 +0530

    citeseer issue resolved

--- utils.py
+++ utils.py
@@ -20,7 +20,16 @@ def load_dataset(path="./data", dataset_str="cora"):
     x, y, tx, ty, allx, ally, graph = tuple(objects)
     test_idx_reorder = parse_index_file(path+"/ind.{}.test.index".format(dataset_str))
     test_idx_range = np.sort(test_idx_reorder)
-
+    if dataset_str == 'citeseer':
+        # Fix citeseer dataset (there are some isolated nodes in the graph)
+        # Find isolated nodes, add them as zero-vecs into the right position
+        test_idx_range_full = range(min(test_idx_reorder), max(test_idx_reorder)+1)
+        tx_extended = sp.lil_matrix((len(test_idx_range_full), x.shape[1]))
+        tx_extended[test_idx_range-min(test_idx_range), :] = tx
+        tx = tx_extended
+        ty_extended = np.zeros((len(test_idx_range_full), y.shape[1]))
+        ty_extended[test_idx_range-min(test_idx_range), :] = ty
+        ty = ty_extended
     features = sp.vstack((allx, tx)).tolil()
     features[test_idx_reorder, :] = features[test_idx_range, :]
     adj = nx.adjacency_matrix(nx.from_dict_of_lists(graph))
